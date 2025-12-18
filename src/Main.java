b4c840e983b405d9f5a3774798cc9cc6937d129d
Sultan <sansyzbaevsultan0510@gmail.com>
2025-12-18 12:02:42 +0500

    1 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
 
         Sport football = new Sport("Football");
         Sport tennis = new Sport("Tennis");
