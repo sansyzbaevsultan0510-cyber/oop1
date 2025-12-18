d864e8ba891a5a1cd09dbb220d4e5427e1164730
Sultan <sansyzbaevsultan0510@gmail.com>
2025-12-18 11:01:40 +0500

    1 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,94 +1,42 @@
 public class Main {
 
     public static void main(String[] args) {
-        Sport football = new Sport("Football", 11);
-        Sport tennis = new Sport("Tennis", 1);
-        Player player1 = new Player("Uali", 18, football);
-        Player player2 = new Player("Yeraly", 18, tennis);
-        SportsClub club = new SportsClub("BDA2509", "Astana");
-        System.out.println(football);
-        System.out.println(tennis);
-        System.out.println(player1);
-        System.out.println(player2);
-        System.out.println(club);
-        System.out.println("Players play the same sport: " +
-                player1.getSport().getName().equals(player2.getSport().getName()));
+
+        Sport football = new Sport("Football");
+        Sport tennis = new Sport("Tennis");
+
+        Player p1 = new Player("Uali", football);
+        Player p2 = new Player("Yeraly", tennis);
+        SportsClub club = new SportsClub("BDA2509");
+        System.out.println(football.name);
+        System.out.println(tennis.name);
+        System.out.println(p1.name + " plays " + p1.sport.name);
+        System.out.println(p2.name + " plays " + p2.sport.name);
+        System.out.println("Club name: " + club.name);
+        if (p1.sport.name.equals(p2.sport.name)) {
+            System.out.println("Players play the same sport");
+        } else {
+            System.out.println("Players play different sports");
+        }
     }
 }
 class Sport {
-    private String name;
-    private int teamSize;
-    public Sport(String name, int teamSize) {
+    String name;
+    Sport(String name) {
         this.name = name;
-        this.teamSize = teamSize;
-    }
-    public String getName() {
-        return name;
-    }
-    public int getTeamSize() {
-        return teamSize;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setTeamSize(int teamSize) {
-        this.teamSize = teamSize;
-    }
-    public String toString() {
-        return "Sport: " + name + ", Team size: " + teamSize;
     }
 }
 class Player {
-    private String name;
-    private int age;
-    private Sport sport;
-    public Player(String name, int age, Sport sport) {
+    String name;
+    Sport sport;
+    Player(String name, Sport sport) {
         this.name = name;
-        this.age = age;
         this.sport = sport;
-    }
-    public String getName() {
-        return name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public Sport getSport() {
-        return sport;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setSport(Sport sport) {
-        this.sport = sport;
-    }
-
-    public String toString() {
-        return "Player: " + name + ", Age: " + age + ", Sport: " + sport.getName();
     }
 }
 class SportsClub {
-    private String clubName;
-    private String city;
-    public SportsClub(String clubName, String city) {
-        this.clubName = clubName;
-        this.city = city;
-    }
-    public String getClubName() {
-        return clubName;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setClubName(String clubName) {
-        this.clubName = clubName;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String toString() {
-        return "Sports Club: " + clubName + ", City: " + city;
+    String name;
+    SportsClub(String name) {
+        this.name = name;
     }
 }
