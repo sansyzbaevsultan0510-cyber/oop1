9f216a6bbbe65d4b8ade8517d004f312a5b0e1a9
Sultan <sansyzbaevsultan0510@gmail.com>
2025-12-19 20:34:30 +0500

    2 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-public class Main {
-
-    public static void main(String[] args ) {
-
+class Main {  // Убрал 'public'
+    public static void main(String[] args ) {  // Параметр 'args' можно удалить, если не используется
         Sport football = new Sport("Football");
         Sport tennis = new Sport("Tennis");
 
         Player p1 = new Player("Uali", football);
         Player p2 = new Player("Yeraly", tennis);
-        SportsClub club = new SportsClub("BDA2509");
+
         System.out.println(football.name);
         System.out.println(tennis.name);
         System.out.println(p1.name + " plays " + p1.sport.name);
         System.out.println(p2.name + " plays " + p2.sport.name);
-        System.out.println("Club name: " + club.name);
+
         if (p1.sport.name.equals(p2.sport.name)) {
             System.out.println("Players play the same sport");
         } else {
@@ -20,23 +18,21 @@ public class Main {
         }
     }
 }
+
 class Sport {
     String name;
+
     Sport(String name) {
         this.name = name;
     }
 }
+
 class Player {
     String name;
     Sport sport;
+
     Player(String name, Sport sport) {
         this.name = name;
         this.sport = sport;
-    }
-}
-class SportsClub {
-    String name;
-    SportsClub(String name) {
-        this.name = name;
     }
 }
